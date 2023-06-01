6217ab7f93947700720fce47722d74fb75fa7031
Sil3ntProgramm3r <130008578+Sil3ntProgramm3r@users.noreply.github.com>
2023-05-31 21:26:07 -0400

    Update Program.cs

--- Prime_numGen/Program.cs
+++ Prime_numGen/Program.cs
@@ -12,60 +12,38 @@ class PrimeNumberGenerator
             string input = Console.ReadLine();
 
             if (string.IsNullOrWhiteSpace(input))
-            {
                 break;
-            }
 
-            if (int.TryParse(input, out int number))
+            if (int.TryParse(input, out int number) && number > 1)
             {
-                if (number <= 1)
-                {
-                    Console.WriteLine("Enter a number greater than 1.");
-                    continue;
-                }
-
                 Console.WriteLine("Prime numbers:");
-
-                for (int i = 2; i <= number; i++)
-                {
-                    if (IsPrime(i))
-                    {
-                        Console.WriteLine(i);
-                    }
-                }
+                PrintPrimeNumbers(number);
             }
             else
             {
-                Console.WriteLine("Invalid input. Please enter a valid number.");
+                Console.WriteLine("Invalid input. Please enter a valid number greater than 1.");
             }
+        }
+    }
+
+    static void PrintPrimeNumbers(int number)
+    {
+        for (int i = 2; i <= number; i++)
+        {
+            if (IsPrime(i))
+                Console.WriteLine(i);
         }
     }
 
     static bool IsPrime(int number)
     {
         if (number < 2)
-        {
             return false;
-        }
 
-        if (number == 2 || number == 3)
-        {
-            return true;
-        }
-
-        if (number % 2 == 0)
-        {
-            return false;
-        }
-
-        int sqrt = (int)Math.Sqrt(number);
-
-        for (int i = 3; i <= sqrt; i += 2)
+        for (int i = 2; i * i <= number; i++)
         {
             if (number % i == 0)
-            {
                 return false;
-            }
         }
 
         return true;
